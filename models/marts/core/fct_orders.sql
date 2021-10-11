9b6e5794b18510f3e7ae944e7cf01de9dfe22f0c
munikrishna.s@outlook.com <munikrishna.s@outlook.com>
2021-10-11 15:38:48 +0000

    tt

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,19 +1,30 @@
-with payment as (
-
-    select * from {{ ref('stg_payment')}}
-
-),
-customers as (
-
-    select * from {{ ref('dim_customers') }}
-
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
-orders as ( payment.id,
-           customer.customer_id,
-           sum(payment.amount) as Amount
-    from payment 
-    left join customers on payment.id = customer.customer_id
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select * from orders
+select * from final
